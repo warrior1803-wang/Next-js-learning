e721a60506b92acf0331fb79035c5d29b1485950
warrior1803-wang <60429051+warrior1803-wang@users.noreply.github.com>
2024-02-05 21:19:37 +0800

    dashboard

---
+++ nextjs-dashboard/.history/app/ui/fonts_20240204213114.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Lustria } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lustria({ subsets: ['latin'] });
---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  /// normal 400 bold 700
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
